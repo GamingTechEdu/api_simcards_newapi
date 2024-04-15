9aa23d4ea692d717010ea7604bda69b100f0e53c
gamingtechedu <gamingtechedu@gmail.com>
2024-04-15 12:09:17 -0300

    commit teste 2

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.19
 require (
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/gorilla/mux v1.8.0
+	golang.org/x/time v0.5.0
 )
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"golang.org/x/time/rate"
 
 	"kdl_api_rest_internal/headers"
 	routesDelete "kdl_api_rest_internal/routes/routes_delete"
@@ -19,6 +20,20 @@ func main() {
 	routesGet.SetGetRoutes(router)
 	routesPost.SetPostRoutes(router)
 	routesDelete.SetDeleteRoutes(router)
+
+	// Criar um limitador de taxa com 1000 requisições por segundo
+	limiter := rate.NewLimiter(rate.Limit(1000), 1000)
+
+	// Adicionar middleware para rate limiting
+	router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if !limiter.Allow() {
+				http.Error(w, "Too many requests", http.StatusTooManyRequests)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	})
 
 	log.Fatal(http.ListenAndServe(":60060", headers.AddCorsHeaders(router)))
 }
